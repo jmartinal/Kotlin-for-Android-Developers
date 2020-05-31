bfd8b53317a3c8076a1c4ffbd6a7f641feb2b9df
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2020-05-31 19:28:25 +0200

    Functions exercise

--- app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
+++ app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jmartinal.kotlinakademy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast("Hello world")
+    }
+
+    // Exercise: Create a fun to toast a message
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
