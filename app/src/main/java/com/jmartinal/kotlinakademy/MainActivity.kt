8250649e5f3eddea52a70979d8723be00cdc98ae
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2020-06-02 19:19:58 +0200

    String templates exercise

--- app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
+++ app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.jmartinal.kotlinakademy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Exercise 2: define a variable to retrieve the "Hello World!" TextView and modify its text
-        val textView = findViewById<TextView>(R.id.message)
-        textView.text = "Hello Kotliner!"
+        // Exercise 3: Create a button and an EditText ant show a toast with the text when clicked
+        val nameET = findViewById<EditText>(R.id.et_name)
+        val button = findViewById<Button>(R.id.btn_greeting)
 
-        toast("Hello world")
+        button.setOnClickListener { toast("Hello ${nameET.text}") }
     }
 
     // Exercise 1: Create a fun to toast a message
