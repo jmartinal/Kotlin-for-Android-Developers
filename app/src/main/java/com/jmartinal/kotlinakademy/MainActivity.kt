483d7c3255d9819d7ed91e107a2278b51f8a9f2f
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2020-06-02 18:58:31 +0200

    Variables exercise

--- app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
+++ app/src/main/java/com/jmartinal/kotlinakademy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jmartinal.kotlinakademy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Exercise 2: define a variable to retrieve the "Hello World!" TextView and modify its text
+        val textView = findViewById<TextView>(R.id.message)
+        textView.text = "Hello Kotliner!"
+
         toast("Hello world")
     }
 
-    // Exercise: Create a fun to toast a message
+    // Exercise 1: Create a fun to toast a message
     private fun toast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
