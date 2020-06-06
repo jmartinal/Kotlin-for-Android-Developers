036b84baebd1f1e53ff58c25245dd867b995c8be
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2020-06-06 18:10:32 +0200

    Extension functions exercise

---
+++ app/src/main/java/com/jmartinal/kotlinakademy/Extensions.kt
@@ -0,0 +1,27 @@
+package com.jmartinal.kotlinakademy
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+fun RecyclerView.ViewHolder.toast(title: String) {
+    itemView.context.toast(title)
+}
+
+fun ViewGroup.inflate(@LayoutRes layoutId: Int): View = LayoutInflater
+    .from(context)
+    .inflate(layoutId, this, false)
+
+fun ImageView.loadUrl(url: String) {
+    Glide.with(this).load(url).into(this)
+}
